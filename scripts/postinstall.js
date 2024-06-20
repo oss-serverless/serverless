4119b76a8686764c5821dc4d0e4e255db6ce99f6
Matthieu Napoli <matthieu@mnapoli.fr>
2024-06-20 11:40:48 +0200

    Remove autoupdate

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -7,7 +7,6 @@ const isWindows = process.platform === 'win32';
 
 const truthyStr = (val) => val && !['0', 'false', 'f', 'n', 'no'].includes(val.toLowerCase());
 const { CI, ADBLOCK, SILENT } = process.env;
-const isNpmGlobalPackage = require('../lib/utils/npm-package/is-global');
 
 if (!truthyStr(CI) && !truthyStr(ADBLOCK) && !truthyStr(SILENT)) {
   const messageTokens = ['Serverless Framework successfully installed!'];
@@ -18,10 +17,6 @@ if (!truthyStr(CI) && !truthyStr(ADBLOCK) && !truthyStr(SILENT)) {
     );
   } else {
     messageTokens.push('To start your first project run “serverless”.');
-  }
-
-  if ((isStandaloneExecutable && !isWindows) || isNpmGlobalPackage()) {
-    messageTokens.push('Turn on automatic updates by running “serverless config --autoupdate”.');
   }
 
   if (isStandaloneExecutable && !isWindows) {
--- test/unit/lib/plugins/config.test.js
+++ test/unit/lib/plugins/config.test.js
@@ -2,8 +2,6 @@
 
 const fs = require('fs');
 const BbPromise = require('bluebird');
-const { expect } = require('chai');
-const config = require('@serverless/utils/config');
 const runServerless = require('../../../utils/run-serverless');
 
 BbPromise.promisifyAll(fs);
@@ -15,25 +13,4 @@ describe('Config', () => {
       command: 'config credentials',
       options: { provider: 'aws', key: 'foo', secret: 'bar' },
     }));
-
-  it('should turn on autoupdate with "--autoupdate"', async () => {
-    await runServerless({
-      cwd: require('os').homedir(),
-      command: 'config',
-      options: { autoupdate: true },
-      modulesCacheStub: {
-        './lib/utils/npm-package/is-global.js': async () => true,
-        './lib/utils/npm-package/is-writable.js': async () => true,
-      },
-    });
-    expect(config.get('autoUpdate.enabled')).to.be.true;
-  });
-  it('should turn off autoupdate with "--no-autoupdate"', async () => {
-    await runServerless({
-      cwd: __dirname,
-      command: 'config',
-      options: { autoupdate: false },
-    });
-    expect(config.get('autoUpdate.enabled')).to.be.false;
-  });
 });
