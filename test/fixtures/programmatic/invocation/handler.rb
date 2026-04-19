cbc41c09de2ae969454774b07fd1a6f136b823e5
Graham Campbell <hello@gjcampbell.co.uk>
2026-04-19 01:06:55 +0100

    Fix flakey test on Windows

--- test/fixtures/programmatic/invocation/handler.rb
+++ test/fixtures/programmatic/invocation/handler.rb
@@ -1,6 +1,10 @@
 require 'json'
 
 def handler(event:, context:)
-  {"statusCode" => 200, "body" => {"message" => "Invoked", "env" => ENV.to_hash}.to_json }
+  env_subset = ENV.to_hash.select { |k, _|
+    k.start_with?('AWS_', 'LAMBDA_', 'SLS_', 'PROVIDER_LEVEL_VAR', 'FUNCTION_LEVEL_VAR',
+                   'PARAM_ENV_VAR', 'NULL_VAR', 'IS_LOCAL', 'NODE_PATH', 'LD_LIBRARY_PATH', 'PATH')
+  }
+  {"statusCode" => 200, "body" => {"message" => "Invoked", "env" => env_subset}.to_json }
 end
 
