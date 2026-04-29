430e7ce87a5c2339338c6f6f7b1eb8f0b411d622
Graham Campbell <hello@gjcampbell.co.uk>
2026-04-29 21:46:56 +0100

    Restore stack trace limit in signal tests

--- test/unit/scripts/serverless-signals.test.js
+++ test/unit/scripts/serverless-signals.test.js
@@ -28,6 +28,11 @@ describe('test/unit/scripts/serverless-signals.test.js', () => {
   let timerToken;
   let stubs;
   let originalPlatformDescriptor;
+  let originalStackTraceLimit;
+
+  beforeEach(() => {
+    originalStackTraceLimit = Error.stackTraceLimit;
+  });
 
   const loadScript = ({ signals = ['SIGINT', 'SIGTERM'], listenerCount = 0 } = {}) => {
     sleepDeferred = deferred();
@@ -103,6 +108,8 @@ describe('test/unit/scripts/serverless-signals.test.js', () => {
       Object.defineProperty(process, 'platform', originalPlatformDescriptor);
       originalPlatformDescriptor = null;
     }
+
+    Error.stackTraceLimit = originalStackTraceLimit;
 
     sinon.restore();
   });
