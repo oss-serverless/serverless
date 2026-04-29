9379c02f59a15d6786ad3571e7176bf2cfdcf76e
Graham Campbell <hello@gjcampbell.co.uk>
2026-04-29 21:34:40 +0100

    Cover require-uncached cache restoration paths

--- test/unit/test-utils/require-uncached.test.js
+++ test/unit/test-utils/require-uncached.test.js
@@ -4,11 +4,64 @@ const { expect } = require('chai');
 const requireUncached = require('../../utils/require-uncached');
 
 describe('test/utils/require-uncached', () => {
-  it('restores the require cache after generic thenables settle', async () => {
-    const modulePath = '../../../package.json';
-    const moduleId = require.resolve(modulePath);
+  const modulePath = '../../../package.json';
+  let moduleId;
+  let originalCacheEntry;
+
+  beforeEach(() => {
+    moduleId = require.resolve(modulePath);
     require(modulePath);
-    const originalCacheEntry = require.cache[moduleId];
+
+    originalCacheEntry = require.cache[moduleId];
+  });
+
+  it('restores the require cache after synchronous callbacks return', () => {
+    let wasCacheCleared = false;
+
+    const result = requireUncached(() => {
+      wasCacheCleared = !require.cache[moduleId];
+
+      return 'result';
+    });
+
+    expect(result).to.equal('result');
+    expect(wasCacheCleared).to.equal(true);
+    expect(require.cache[moduleId]).to.equal(originalCacheEntry);
+  });
+
+  it('restores the require cache after synchronous callbacks throw', () => {
+    const error = new Error('Failure');
+    let wasCacheCleared = false;
+
+    expect(() =>
+      requireUncached(() => {
+        wasCacheCleared = !require.cache[moduleId];
+
+        throw error;
+      })
+    ).to.throw(error);
+
+    expect(wasCacheCleared).to.equal(true);
+    expect(require.cache[moduleId]).to.equal(originalCacheEntry);
+  });
+
+  it('restores the require cache after promises reject', async () => {
+    const error = new Error('Failure');
+    let wasCacheCleared = false;
+
+    await expect(
+      requireUncached(() => {
+        wasCacheCleared = !require.cache[moduleId];
+
+        return Promise.reject(error);
+      })
+    ).to.be.rejectedWith(error);
+
+    expect(wasCacheCleared).to.equal(true);
+    expect(require.cache[moduleId]).to.equal(originalCacheEntry);
+  });
+
+  it('restores the require cache after generic thenables settle', async () => {
     let wasCacheCleared = false;
 
     const result = await requireUncached(() => ({
