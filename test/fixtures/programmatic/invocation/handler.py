75fd03eb0a78dc0acc7afa67fa25727e7abc90c2
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2026-06-10 22:17:12 +0100

    Fix flaky invoke tests (#359)

--- test/fixtures/programmatic/invocation/handler.py
+++ test/fixtures/programmatic/invocation/handler.py
@@ -1,12 +1,17 @@
 import json
 import os
 
+ENV_KEY_PREFIXES = ('AWS_', 'LAMBDA_', 'SLS_', 'PROVIDER_LEVEL_VAR', 'FUNCTION_LEVEL_VAR',
+                    'PARAM_ENV_VAR', 'NULL_VAR', 'IS_LOCAL', 'NODE_PATH', 'LD_LIBRARY_PATH',
+                    'PATH', 'LANG')
+
 
 def handler(event, context):
+    env_subset = {k: v for k, v in os.environ.items() if k.startswith(ENV_KEY_PREFIXES)}
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "Invoked",
-            "env": dict(os.environ)
+            "env": env_subset
         })
     }
