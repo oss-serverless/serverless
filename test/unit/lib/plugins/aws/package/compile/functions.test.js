28aef05a28c6ece09eb57981a48da068305ea2f3
Graham Campbell <hello@gjcampbell.co.uk>
2026-06-10 19:21:00 +0100

    Constrain function name to the Lambda character set

--- test/unit/lib/plugins/aws/package/compile/functions.test.js
+++ test/unit/lib/plugins/aws/package/compile/functions.test.js
@@ -1564,6 +1564,25 @@ describe('lib/plugins/aws/package/compile/functions/index.test.js', () => {
       );
     });
 
+    it('should reject `functions[].name` values outside the Lambda character set', () => {
+      return expect(
+        runServerless({
+          fixture: 'function',
+          command: 'package',
+          configExt: {
+            functions: {
+              basic: {
+                name: 'my.dotted.name',
+              },
+            },
+          },
+        })
+      ).to.eventually.be.rejected.and.have.property(
+        'code',
+        'INVALID_NON_SCHEMA_COMPLIANT_CONFIGURATION'
+      );
+    });
+
     it('should support `provider.runtimeManagement`', () => {
       const providerConfig = serviceConfig.provider;
 
