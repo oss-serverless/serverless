095a10271287e4e747fbd439280244843281bc3d
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2026-08-04 15:38:40 +0100

    Fix schema rejection of function-level vpc opt-out (#432)
    Co-authored-by: BenjaminBurgalat <b.burgalat@betclicgroup.com>

--- test/unit/lib/plugins/aws/package/compile/functions.test.js
+++ test/unit/lib/plugins/aws/package/compile/functions.test.js
@@ -2588,6 +2588,10 @@ describe('lib/plugins/aws/package/compile/functions/index.test.js', () => {
               vpc: null,
               handler: 'index.handler',
             },
+            vpcDisable: {
+              vpc: false,
+              handler: 'index.handler',
+            },
           },
         },
       });
@@ -2635,6 +2639,12 @@ describe('lib/plugins/aws/package/compile/functions/index.test.js', () => {
 
     it('should allow `functions[].vpc` to specify no vpc', () => {
       const Properties = cfResources[naming.getLambdaLogicalId('vpcNullify')].Properties;
+
+      expect(Properties.VpcConfig).to.be.undefined;
+    });
+
+    it('should allow `functions[].vpc` set to `false` to specify no vpc', () => {
+      const Properties = cfResources[naming.getLambdaLogicalId('vpcDisable')].Properties;
 
       expect(Properties.VpcConfig).to.be.undefined;
     });
